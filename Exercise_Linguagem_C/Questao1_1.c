dc6e3b078bd17788f8580e224da1a2e2eaa640e8
James <james.nascimento@ufrpe.br>
2022-11-08 19:13:55 -0300

    Adição de pastas e livro de exercícios.

---
+++ Exercise_Linguagem_C/Questao1_1.c
@@ -0,0 +1,38 @@
+/**
+<h1>Quetao1_1</h1>
+* Dada uma coleção de números inteiros terminada por 0, imprime seus quadrados.
+* <p>
+* <b>Nota:<b> Leia atentamente a documentação deste programa 
+* para desfrutar dos recursos oferecidos pelo autor.
+* 
+* @author James Anderson
+* @version 1.0
+* @since 08/11/2022
+*/
+
+#include <string.h>;
+#include <math.h>;
+#include <stdlib.h>;
+#include <stdio.h>;
+
+main (void) 
+{
+
+//Declaração das variáveis.
+int tamanhoLista;
+//Solicitação e leitura do número de elementos da lista.
+print ("Digite o número de elementos da lista: ");
+scanf ("%d", &tamanhoLista);
+//Implementação de condição para requerer um novo valor caso o número passado seja inválido.
+while (tamanhoLista < 0) {
+    printf ("O tamanho da lista deve ser maior que zero.\n");
+    //Solicitação e leitura do número de elementos da lista.
+    printf ("Digite o número de elementos da lista: ");
+    scanf ("%d", &tamanhoLista);
+}
+int lista[] = array(tamanhoLista);
+
+
+    return 0;
+}
+
