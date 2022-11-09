b4f34116b057e96703b01d7d6031d8db97214990
James <james.nascimento@ufrpe.br>
2022-11-09 08:15:27 -0300

    Correçãoes realizadas.

--- Exercise_Linguagem_C/Questao1_3.c
+++ Exercise_Linguagem_C/Questao1_3.c
@@ -1,6 +1,6 @@
 /**
-<h1>Questao1_2</h1>
-* Dado n, calcula a soma dos n primeiros numeros inteiros positivos.
+<h1>Questao1_3</h1>
+* Dado n, imprime os n primeiros numeros ímpares positivos.
 * <p>
 * <b>Nota:<b> Leia atentamente a documentação deste programa 
 * para desfrutar dos recursos oferecidos pelo autor.
@@ -19,30 +19,29 @@ int main ()
 {
 //Declaração das variáveis.
 int tamanhoLista;
+int contador = 0;
 //Solicitação e leitura do número de elementos da lista.
 printf ("Digite o numero de elementos da lista: ");
 scanf ("%d", &tamanhoLista);
-//Implementação de condição para requerer um novo valor caso o número passado seja inválido.
-while (tamanhoLista < 0) {
+/**Implementação de condição para requerer um novo valor 
+*caso o número passado seja inválido.
+*/
+while (tamanhoLista <= 0) {
     printf ("O tamanho da lista deve ser maior que zero.\n");
     //Solicitação e leitura do número de elementos da lista.
     printf ("Digite o numero de elementos da lista: ");
     scanf ("%d", &tamanhoLista);
 }
-//Declaração do array.
-int lista[tamanhoLista];
-//Implementação do laço para preenchimento da lista de inteiros.
-for (int posicao = 0; posicao < tamanhoLista; posicao++) {    
-    printf ("Digite um valor de posicao %d para a lista: ", posicao + 1);
-    scanf ("%d", &lista[posicao]);    
+//Implementação do laço para impressão os inteiros ímpares.
+for (int posicao = 1; posicao < (2*tamanhoLista); posicao++) {
+    //Implementação da condição para impressão dos numeros.
+    contador++;
+    if (posicao%2 == 1 && contador < tamanhoLista) {
+        printf ("%d,", posicao);
+    }else if(posicao%2 == 1 && contador == tamanhoLista) { 
+        printf ("%d.", posicao); 
+    }            
 }
-//Implementação do laço para impressão dos quadrados da lista de inteiros.
-for (int posicao = 0; posicao < tamanhoLista; posicao++) {
-    //Implementação da condição para impressão dos valores ímpares.
-    if (lista[posicao]%2 != 0) {
-        printf ("Valor %d: %d\n", posicao + 1, lista[posicao]);
-    }              
-} 
 return 0;
 }
 
