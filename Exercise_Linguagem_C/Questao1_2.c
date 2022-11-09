779a2c71d2c8e333095a141a91c3ba850c69093e
James <james.nascimento@ufrpe.br>
2022-11-09 07:52:24 -0300

    Programa 2 finalizado.

--- Exercise_Linguagem_C/Questao1_2.c
+++ Exercise_Linguagem_C/Questao1_2.c
@@ -1,13 +1,13 @@
 /**
-<h1>Questao1_1</h1>
-* Dada uma coleção de números inteiros terminada por 0, imprime seus quadrados.
+<h1>Questao1_2</h1>
+* Dado n, calcula a soma dos n primeiros numeros inteiros positivos.
 * <p>
 * <b>Nota:<b> Leia atentamente a documentação deste programa 
 * para desfrutar dos recursos oferecidos pelo autor.
 * 
 * @author James Anderson
 * @version 1.0
-* @since 08/11/2022
+* @since 09/11/2022
 */
 
 #include <string.h>
@@ -20,6 +20,7 @@ int main ()
 
 //Declaração das variáveis.
 int tamanhoLista;
+int soma = 0;
 //Solicitação e leitura do número de elementos da lista.
 printf ("Digite o numero de elementos da lista: ");
 scanf ("%d", &tamanhoLista);
@@ -37,14 +38,20 @@ for (int posicao = 0; posicao < tamanhoLista; posicao++) {
     
     printf ("Digite um valor de posicao %d para a lista: ", posicao+1);
     scanf ("%d", &lista[posicao]);
+    //Implementação da condiçaõ para garantir que o número seja positivo.
+    while (lista[posicao] <= 0) {
+        printf ("O numero informado nao eh valido. Por favor, inserir valor positivo.\n");
+        printf ("Digite um valor de posicao %d para a lista: ", posicao+1);
+        scanf ("%d", &lista[posicao]);
+    }
 }
 //Implementação do laço para impressão dos quadrados da lista de inteiros.
 for (int posicao = 0; posicao < tamanhoLista; posicao++) {
-    //Calcula o quadrado do valor na posicao especificada.
-    int valorQuadrado = pow(lista[posicao],2);
-    //Impressão dos quadrados dos valores da lista.
-    printf ("Valor %d: %d \n", posicao + 1,  valorQuadrado);    
+    //Soma os valores da lista.
+    soma += lista[posicao];          
 }
+//Impressão da soma.    
+printf ("A soma dos valores da lista: %d", soma);  
 return 0;
 }
 
