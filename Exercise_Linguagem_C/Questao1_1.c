55e665aced2d3a8fb44c348be65173df12f0a70d
James <james.nascimento@ufrpe.br>
2022-11-09 05:08:03 -0300

    Programa 1 fializado.

--- Exercise_Linguagem_C/Questao1_1.c
+++ Exercise_Linguagem_C/Questao1_1.c
@@ -10,29 +10,41 @@
 * @since 08/11/2022
 */
 
-#include <string.h>;
-#include <math.h>;
-#include <stdlib.h>;
-#include <stdio.h>;
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-main (void) 
+int main () 
 {
 
 //Declaração das variáveis.
 int tamanhoLista;
 //Solicitação e leitura do número de elementos da lista.
-print ("Digite o número de elementos da lista: ");
+printf ("Digite o numero de elementos da lista: ");
 scanf ("%d", &tamanhoLista);
 //Implementação de condição para requerer um novo valor caso o número passado seja inválido.
 while (tamanhoLista < 0) {
     printf ("O tamanho da lista deve ser maior que zero.\n");
     //Solicitação e leitura do número de elementos da lista.
-    printf ("Digite o número de elementos da lista: ");
+    printf ("Digite o numero de elementos da lista: ");
     scanf ("%d", &tamanhoLista);
 }
-int lista[] = array(tamanhoLista);
-
-
+//Declaração do array.
+int lista[tamanhoLista];
+//Implementação do laço para preenchimento da lista de inteiros.
+for (int posicao = 0; posicao < tamanhoLista; posicao++) {
+    
+    printf ("Digite um valor de posicao %d para a lista: ", posicao+1);
+    scanf ("%d", &lista[posicao]);
+}
+//Implementação do laço para impressão dos quadrados da lista de inteiros.
+for (int posicao = 0; posicao < tamanhoLista; posicao++) {
+    //Calcula o quadrado do valor na posicao especificada.
+    int valorQuadrado = pow(lista[posicao],2);
+    
+    printf ("Valor %d: %d \n", posicao + 1,  valorQuadrado);    
+}
     return 0;
 }
 
