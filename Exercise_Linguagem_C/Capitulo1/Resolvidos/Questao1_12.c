51f38e979e62513c16a398e52ea6147654ba516f
James <james.nascimento@ufrpe.br>
2022-11-10 08:02:53 -0300

    P11 finalizado.

---
+++ Exercise_Linguagem_C/Capitulo1/Resolvidos/Questao1_12.c
@@ -0,0 +1,44 @@
+/**
+<h1>Questao1_11</h1>
+* Dado um número inteiro positivo, verifica se ele é primo.
+* <p>
+* <b>Nota:<b> Leia atentamente a documentação deste programa 
+* para desfrutar dos recursos oferecidos pelo autor.
+* 
+* @author James Anderson
+* @version 1.0
+* @since 09/11/2022
+*/
+
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () 
+{
+//Declaração das variáveis.
+int number, contador; 
+printf ("Digite um numero positivo: ");
+scanf ("%d", &number);
+/**Implementação de condição para requerer um novo valor 
+*caso o numero informado seja inválido.
+*/
+while (number <= 0) {
+    printf ("O valor deve ser maior que zero.\n");        
+    printf ("Digite um numero positivo: ");
+    scanf ("%d", &number);       
+}
+//Implementação do laço para descobrir se o número é primo.
+for (int i = 1; i <= number; i++) {
+    //Condição de verificação de número primo.
+    if(number%i == 0){
+        contador++;
+    }
+}
+if (contador > 2) {
+    printf ("O numero %d eh primo.", number);
+} else printf ("O numero %d nao eh primo.", number);
+    
+return 0;
+}
