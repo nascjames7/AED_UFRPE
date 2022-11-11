65d868cb7d9b7ca32c3f3ddd6451f7b83054c242
James <james.nascimento@ufrpe.br>
2022-11-11 12:10:01 -0300

    Alteração de código.

--- Exercise_Linguagem_C/Capitulo1/Resolvidos/Quetao1_16.c
+++ Exercise_Linguagem_C/Capitulo1/Resolvidos/Quetao1_16.c
@@ -1,6 +1,6 @@
 /**
 <h1>Questao1_16</h1>
-* Dado um número natural na base binária, transforma para a base decimal.* 
+* Dado um número natural na base binária, transforma para a base decimal. 
 * <p>
 * <b>Nota:<b> Leia atentamente a documentação deste programa 
 * para desfrutar dos recursos oferecidos pelo autor.
@@ -23,25 +23,16 @@ int main ()
   //Solicitação e leitura dos números.
   //Solicitação e leitura dos bits.
     printf ("Digite o numero: ");
-    scanf ("%s", palavraBinaria);
-    //Imprime a palavra binária.
-    //puts(palavraBinaria);
-    //Imprime o número de caracteres da palavra.
-    //printf ("%d\n", strlen(palavraBinaria));     
+    scanf ("%s", palavraBinaria);         
   //Implementação do laço para o cálculo do número decimal.
-  for (int i = 0; i < strlen(palavraBinaria); i++) {
-    //Chamada recursiva.
-    int exp = pow(2, i);
-    //printf ("exp: %d\n", exp);
-    //printf ("palavraBinaria: %d\n", (int)palavraBinaria[i]);
+  for (int i = 0; i < strlen(palavraBinaria); i++) {       
     //Implementação das condições para transformar o caracter da Tabela ASCII em binário.
     if ((int)palavraBinaria[i] == 48) {
       numberBinario = 0;
     } else if ((int)palavraBinaria[i] == 49) {
       numberBinario = 1;
     } 
-    decimal += numberBinario*pow(2, strlen(palavraBinaria) - 1 - i);
-    printf ("decimal: %d\n", decimal);
+    decimal += numberBinario*pow(2, strlen(palavraBinaria) - 1 - i);    
   }
   printf ("O numero decimal: %d\n", decimal);
   return 0;
