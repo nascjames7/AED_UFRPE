40f7781ea9c3a7a498701e3f3a655797a3a49386
James <james.nascimento@ufrpe.br>
2022-11-16 09:57:34 -0300

    Programa 5 finalizado.

--- Exercise_Linguagem_C/Capitulo2/Resolvidas/Questao2_5.c
+++ Exercise_Linguagem_C/Capitulo2/Resolvidas/Questao2_5.c
@@ -19,45 +19,46 @@
 int main () 
 {
     //Declaração das variáveis.
-    int soma = 0, number, contador = 0;    
+    int soma = 0, limiteSuperior, contador = 0;    
     //Solicitaçao e leitura da variável.
-    printf ("Digite o numero de valores da sequencia: \n");
-    scanf ("%d", &number);
+    printf ("Digite o limite superior do intervalo: \n");
+    scanf ("%d", &limiteSuperior);
     //Implementação da condição para leitura de um valor válido.
-    while (number <= 0) {
+    while (limiteSuperior <= 0) {
         printf ("Digite o numero de valores da sequencia: \n");
-        scanf ("%d", &number);
+        scanf ("%d", &limiteSuperior);
     }
-    //Declaração do array para guardar a sequancia.
-    int sequencia[number];
+    //Declaração do array para guardar a sequência.
+    int sequencia[limiteSuperior];
     //Impementação do laço para o preenchimento do array.
-    for (int i = 0; i < number; i++) {
-        printf ("Digite um valor: ");
-        scanf ("%d", &sequencia[i]);        
-    }
-    //Impementação do laço para o cálculo da soma.
-    for (int i = 0; i < number; i++) {
-        printf ("i: %d\n", i);
-        //Implementação do laço para o cálculo da soma.
-        for (int position = 1; position <= sequencia[i]; position++) {
-            //Implementação da condição para incrementar o contador.
-            if (sequencia[i]%position == 0) {
-                printf ("sequencia[%d]: %d\n", i, sequencia[i]);
-                //Incremento do contador.
-                contador++;
-                printf ("contador: %d\n", contador);
-            }
+    for (int i = 0; i < limiteSuperior; i++) {
+        //Implementação das condições.
+        if (limiteSuperior == 1 && i == 0) {
+            sequencia[0] = 1;
+            soma += sequencia[0];      
         }
-        //Implementação da condição para verificar se o número é primo.                
-        if (contador == 2) {
-            //Definição da soma.
-            soma += sequencia[i];
-            //printf ("soma: %d\n", soma);            
+        if (limiteSuperior != 1 && i == 0) {
+            sequencia[0] = 1 + limiteSuperior*(limiteSuperior - 1);
+            soma += sequencia[0];           
         }
-        //Zera o contador.
-        contador = 0;
-    }        
-    //Impressão do somatório.
-    printf ("A soma: %d", soma);     
+        if (limiteSuperior != 1 && i != 0) {
+            sequencia[i] = sequencia[i - 1] + 2;
+            soma += sequencia[i];            
+        }
+    }          
+    //Dclaração da potência cúbica do limiteSuperior.
+    int potencia = pow(limiteSuperior, 3);
+    //Impressão da sequencia.
+    printf ("%d = ", potencia);
+    for (int i = 0; i < limiteSuperior; i++) {
+        //Para valores que estejam antes da última posição.
+        if (i < (limiteSuperior - 1)) {
+            printf ("%d + ", sequencia[i]);
+        }
+        //Para a última posição.
+        if (i == (limiteSuperior - 1)) {
+            printf ("%d = %d", sequencia[i], soma);
+        }
+    }         
     return 0;
 }
