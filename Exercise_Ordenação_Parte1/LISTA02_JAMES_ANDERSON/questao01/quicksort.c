eb1dd34cf88d0eccd3138c1e87a6474369f8ffce
James <james.nascimento@ufrpe.br>
2022-12-14 16:08:05 -0300

    QuickSort em desenvolvimento.

--- Exercise_Ordenação_Parte1/LISTA02_JAMES_ANDERSON/questao01/quicksort.c
+++ Exercise_Ordenação_Parte1/LISTA02_JAMES_ANDERSON/questao01/quicksort.c
@@ -1,4 +1,4 @@
-**
+/**
 <h1>QuickSort</h1>
 * Dada uma coleção de números inteiros terminada, ordena-los.
 * <p>
@@ -30,27 +30,39 @@ void printArray(int vetor[], int tamanhoOriginal) {
     }
 }
 
+//Procedimento de troca.
+void trocar(int first, int second) {
+    int temp = first;
+    first = second;
+    second = temp;
+}
 
 
-/* Function to sort an array using insertion sort*/
-void quickSort(int vetor[], int tamanhoOriginal) {
-  int i, j, valorMinimo;
-  for (int i = 1; i < tamanhoOriginal; i++) {
-    valorMinimo = i;
-    for (int j = i + 1; j <= tamanhoOriginal; j++) {
-        if (vetor[j] < vetor[valorMinimo]) {
-            valorMinimo = j;
-            int auxiliar1 = &vetor[i];
-            int auxiliar2 = &vetor[valorMinimo];
-            
-            //printf ("Aqui");
-            //troca(vetor, j, valorMinimo);
-            //printf ("Sai");
+//Procedimento que realiza a partição.
+int particionar(int vetor[], int tamanhoParticao, int tamanhoOriginal) {
+    int auxiliar = vetor[tamanhoOriginal - 1];
+    int i = tamanhoParticao;
+    for (int j = (tamanhoParticao + 1); j < tamanhoOriginal; j++) {
+        if (vetor[j] < auxiliar) {
+            //Incremento de i.
+            i++;
+            trocar(vetor[i], vetor[j]);
         }
     }
+    trocar(vetor[i + 1], vetor[tamanhoOriginal]);
+    return (i + 1);
+}
+
+/* Function to sort an array using quicksort*/
+void quickSort(int vetor[], int tamanhoParticao, int tamanhoOriginal) {
+  if (tamanhoParticao < tamanhoOriginal) {
+    //Declaração da variavel quick (representa o retorno da função particionar).
+    int quick = particionar(vetor, tamanhoParticao, tamanhoOriginal);
+    //Chamada recursiva do procedimento quickSort.
+    quickSort(vetor, tamanhoParticao, (quick - 1));
+    quickSort(vetor, (quick + 1), tamanhoOriginal);
   }
 }  
-
 
 /* Driver program to test insertion sort */
 int main() {
@@ -60,13 +72,14 @@ int main() {
   int tamanhoOriginal;
   printf("Enter the size of array: ");
   scanf("%d", &tamanhoOriginal);
+  int quickInit = tamanhoOriginal/2;
   int vetor[tamanhoOriginal];
   for (int i = 0; i < tamanhoOriginal; i++) {
     vetor[i] = rand() % 100;
   }
 
   printArray(vetor, tamanhoOriginal);
-  quickSort(vetor, tamanhoOriginal);
+  quickSort(vetor, quickInit, tamanhoOriginal);
   printArray(vetor, tamanhoOriginal);
 
   return 0;
