2ec5c01d6eab84e6df0eafb45c86d0ef7993c46a
James <james.nascimento@ufrpe.br>
2022-12-16 11:41:57 -0300

    Modificações.

--- Programas de Ordenação/Ordenação/insertionSortRecursivo/insertionSortRecursivo.c
+++ Programas de Ordenação/Ordenação/insertionSortRecursivo/insertionSortRecursivo.c
@@ -32,8 +32,10 @@ void printArray(int vetor[], int tamanhoOriginal) {
 
 /* Function to sort an array using insertion sort*/
 void insertionSort(int vetor[], int tamanhoOriginal) {
+  int contador = 0;
   int i, key, j;
   for (i = 1; i < tamanhoOriginal; i++) {
+    contador++;
     key = vetor[i];
     j = i - 1;
 
@@ -41,6 +43,7 @@ void insertionSort(int vetor[], int tamanhoOriginal) {
       greater than key, to one position ahead
       of their current position */
     while (j >= 0 && vetor[j] > key) {
+        contador++;
       //Troca das posições (j + 1 = i).
       vetor[j + 1] = vetor[j];
       //Decremento de j.
@@ -48,24 +51,26 @@ void insertionSort(int vetor[], int tamanhoOriginal) {
     }
     vetor[j + 1] = key;
   }
+  printf ("contador: %d", contador);
 }
 
 /* Driver program to test insertion sort */
 int main() {
-  // int vetor[] = { 12, 11, 13, 5, 6 };
+  //int vetor[] = { 12, 13, 14, 15, 16};
+  int vetor[] = { 22, 20, 3, 2, 1};
   // int tamanho = sizeof(vetor) / sizeof(vetor[0]);
 
-  int tamanhoOriginal;
-  printf("Enter the size of array: ");
+  int tamanhoOriginal = 5;
+  /*printf("Enter the size of array: ");
   scanf("%d", &tamanhoOriginal);
   int vetor[tamanhoOriginal];
   for (int i = 0; i < tamanhoOriginal; i++) {
     vetor[i] = rand() % 100;
-  }
+  }*/
 
   printArray(vetor, tamanhoOriginal);
   insertionSort(vetor, tamanhoOriginal);
-  printArray(vetor, tamanhoOriginal);
+  printArray(vetor, tamanhoOriginal);  
 
   return 0;
 }
